94d6d9031ff01e99ef5ac83605feea01f0ee3402
LBJ <LBJ>
2017-07-29 14:36:21 +0800

    20170729

---
+++ ajax0403.php
@@ -0,0 +1,12 @@
+<?php header("Content-Type: text/xml ; charset=''UTF-8")?>
+<employees>
+    <employee>
+        <firstName>喔喔喔</firstName> <lastName>Doe</lastName>
+    </employee>
+    <employee>
+        <firstName>Anna</firstName> <lastName>Smith</lastName>
+    </employee>
+    <employee>
+        <firstName>Peter</firstName> <lastName>Jones</lastName>
+    </employee>
+</employees>
--- test.php
+++ test.php
@@ -1,17 +1,18 @@
 
-<?php
-    $req = $_SERVER;
-//    foreach($req as $key => $value){
-//        echo "{$key} = {$value} <br/>";
-//    }
-//    echo $_SERVER['HTTP_USER_AGENT'];
-
-    if($agent == 'ming'){
-         echo 'hello ming';
-    }else if(strpos($agent,'Chrome')!= false){
-         echo 'hello chrome browser';
-    }else{
-        echo 'hello browser';
-    }
-
-?>
+   <?php
+       $req = $_SERVER;
+   //    foreach ($req as $key => $value){
+   //        echo "{$key} = {$value}<br />";
+   //    }
+       $agent =  $_SERVER['HTTP_USER_AGENT'];
+       if ($agent == 'ming'){
+           //
+           echo 'Hello, Brad';
+       }else if (strpos($agent, 'Chrome') !== false){
+           //
+           echo 'Hello, Chrome';
+       }else {
+           //
+           echo 'Hello, Browser';
+       }
+   ?>
