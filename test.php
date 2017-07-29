1d03b6c5c3f568d832c79a0addec408e77e0c378
LBJ <LBJ>
2017-07-29 11:32:02 +0800

    20170729

--- test.php
+++ test.php
@@ -1,6 +1,16 @@
-<?php echo 'hello world';?>
+
 <?php
-
-
+    $req = $_SERVER;
+//    foreach($req as $key => $value){
+//        echo "{$key} = {$value} <br/>";
+//    }
+//    echo $_SERVER['HTTP_USER_AGENT'];
+    if($agent == 'ming'){
+         echo 'hello ming';
+    }else if(strpos($agent,'Chrome')!= false){
+         echo 'hello chrome browser';
+    }else{
+        echo 'hello browser';
+    }
 
 ?>
